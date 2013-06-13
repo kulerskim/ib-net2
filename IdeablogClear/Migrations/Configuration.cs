d004d266712fc4f4d2cb27d18f9d9f4a1559dada
Mariusz Kulerski <mariusz.kulerski@millwardbrown.com>
2013-06-13 13:49:46 +0200

    Added database initial seed

--- IdeablogClear/Migrations/Configuration.cs
+++ IdeablogClear/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace IdeablogClear.Migrations
 {
+    using IdeablogClear.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -26,6 +27,10 @@ namespace IdeablogClear.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            context.Users.AddOrUpdate(
+                u => u.Name,
+                new User { Name = "Jan Testowy", Language = "he" }
+            );
         }
     }
 }
